798adde4b30a811421f488918257ea57f61d2b82
Waqar Detho <waqaralidetho@hotmail.com>
2019-02-26 21:53:06 +0100

    feat(Path): Read path from the called class using annotation

--- src/main/java/NewHTTPServer.java
+++ src/main/java/NewHTTPServer.java
@@ -5,25 +5,57 @@ import com.sun.net.httpserver.HttpServer;
 import java.io.IOException;
 import java.net.InetSocketAddress;
 
+/**
+ * Base class to handle HTTP Request. In order to distinguish it from
+ * HTTPServer JDK class the name is changed to NEWHTTPServer
+ */
+
 public class NewHTTPServer {
-	private int port;
-	private HttpServer server;
+    private int port = 8989;
+    private HttpServer server;
 
-	public void Start(int port) {
-		try {
-			this.port = port;
-			server = HttpServer.create(new InetSocketAddress(port), 0);
-			System.out.println("server started at " + port);
-			server.createContext("/", new Handlers.RootHandler());
-			server.setExecutor(null);
-			server.start();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-	}
+    public NewHTTPServer() {
+        try {
+            this.port = port;
+            server = HttpServer.create(new InetSocketAddress(port), 0);
+            System.out.println("server started at " + port);
+            server.createContext(getPathFromTheRequest(), new Handlers.RootHandler());
+            server.setExecutor(null);
+            server.start();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 
-	public void Stop() {
-		server.stop(0);
-		System.out.println("server stopped");
-	}
+    private String getPathFromTheRequest() {
+        String result = "";
+        try {
+            Class callerClass = getCallerClass(3);
+            Path path = (Path) callerClass.getAnnotation(Path.class);
+            result = ((path != null) ? path.value() : "/");
+        } catch (ClassNotFoundException ex) {
+            System.err.println("Cannot read provided server path");
+        }
+        return result;
+    }
+
+    public void Stop() {
+        server.stop(0);
+        System.out.println("server stopped");
+    }
+
+
+    /**
+     * In order to read the path from the calling class
+     * to create the server accordingly.
+     * @param level
+     * @return
+     * @throws ClassNotFoundException
+     */
+    public static Class getCallerClass(int level) throws ClassNotFoundException {
+        StackTraceElement[] stElements = Thread.currentThread().getStackTrace();
+        String rawFQN = stElements[level + 1].toString().split("\\(")[0];
+        return Class.forName(rawFQN.substring(0, rawFQN.lastIndexOf('.')));
+    }
 }
+
