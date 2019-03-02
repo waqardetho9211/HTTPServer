1fbfd8ddf56e215b933034b1383741532408a3c2
Waqar Detho <waqaralidetho@hotmail.com>
2019-03-02 15:49:22 +0100

    feat: Amend response class
    - set it up through request

--- src/main/java/Response/HTTPResponse.java
+++ src/main/java/Response/HTTPResponse.java
@@ -2,18 +2,18 @@ package main.java.Response;
 
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
+import lombok.Getter;
+import lombok.Setter;
 
 import java.io.IOException;
 import java.io.OutputStream;
 
-public class HTTPResponse implements HttpHandler {
-    @Override
-    public void handle(HttpExchange httpExchange) throws IOException {
-        String response = "this is a basic http response";
-        httpExchange.sendResponseHeaders(200, response.length());
-        OutputStream outputStream = httpExchange.getResponseBody();
-        outputStream.write(response.getBytes());
-        outputStream.close();
+public class HTTPResponse {
+    @Getter
+    @Setter
+    private String responseString;
 
+    public void setResponseString(String response) {
+        this.responseString = response;
     }
 }
