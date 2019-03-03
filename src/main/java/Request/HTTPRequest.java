77209bf8aa02e286f43c4ee8f5289b7adf20cbe4
Waqar Detho <waqaralidetho@hotmail.com>
2019-03-03 11:55:55 +0100

    refactor: Separation of concerns
    - for better code readability.

--- src/main/java/Request/HTTPRequest.java
+++ src/main/java/Request/HTTPRequest.java
@@ -1,27 +1,15 @@
 package main.java.Request;
 
-import com.sun.net.httpserver.HttpServer;
-import main.java.Response.HTTPResponse;
-
-import java.io.IOException;
-import java.net.InetSocketAddress;
+import lombok.Getter;
+import lombok.Setter;
 
 public class HTTPRequest {
-    public HttpServer server;
-    private int port = 8989;
-
+    @Getter
+    @Setter
+    public String connectionPath;
+    @Getter
+    @Setter
+    public HTTPRequestType httpRequestType;
     public HTTPRequest() {}
 
-    public HTTPRequest setupRequest(String connectionPath, HTTPRequestType type, HTTPResponse response){
-        try {
-            server = HttpServer.create(new InetSocketAddress(port), 0);
-        } catch (IOException e) {
-            System.err.println("Unable to create HTTP Connection");
-        }
-        HTTPRequestFactory httpRequestFactory = new HTTPRequestFactory();
-        server = httpRequestFactory.getRequest(this.server, type, response, connectionPath);
-        server.setExecutor(null);
-        server.start();
-        return this;
-    }
 }
