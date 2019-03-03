72b28270edc194bfce9f0359719cd71a4f9ed225
Waqar Detho <waqaralidetho@hotmail.com>
2019-03-03 11:55:06 +0100

    feat: Add ETAG in HTTP headers (WIP)

--- src/main/java/header/HTTPHeaders.java
+++ src/main/java/header/HTTPHeaders.java
@@ -1,4 +1,13 @@
 package main.java.header;
 
+import lombok.Getter;
+import lombok.Setter;
+
 public class HTTPHeaders {
+    @Getter
+    @Setter
+    public String ETAG;
+
+    public HTTPHeaders() {
+    }
 }
